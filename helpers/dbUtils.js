7f362df3a5805c0f6c39913dd616bb95dd833484
Travis Bates <tbateswsu@gmail.com>
2022-05-10 20:09:24 -0700

    added helpers file and dbUtils.js file that will contain helper functions, including outline of operations required

---
+++ helpers/dbUtils.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "XC04stareld57*",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+// INCLUDE THE FOLLOWING OPERATIONS (plus BONUS)
+// View all departments
+// View all roles
+// View all employees
+// Veiw employees by manager (BONUS)
+// View employees by department (BONUS)
+// View sum of salaries by department (BONUS)
+
+// Add department
+// Add role
+// Add employee
+
+// Update an employee role
+// Update employee managers (BONUS)
+
+// Delete department (BONUS)
+// Delete role (BONUS)
+// Delete employee (BONUS)
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection(
   {
     host: "localhost",
     user: "root",
-    password: "pw*",
+    password: "XC04stareld57*",
     database: "employees_db",
   },
   console.log(`Connected to the employees_db database.`)
