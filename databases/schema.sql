fe17a846bdd49b2baabaa767596c9cfbc65e2eb9
Travis Levi B <tbateswsu@gmail.com>
2022-05-14 06:05:11 -0700

    fixed errors in database syntax

--- databases/schema.sql
+++ databases/schema.sql
@@ -1,31 +1,34 @@
-DROP DATABASE IF EXISTS employees_db: CREATE DATABASE employees_db;
+DROP DATABASE IF EXISTS employees_db; 
+
+CREATE DATABASE employees_db;
 
 USE employees_db;
 
 CREATE TABLE department (
-    id: INT NOT NULL AUTO_INCREMENT = 10,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id: INT NOT NULL AUTO_INCREMENT = 100,
-    title: VARCHAR(30) NOT NULL,
-    salary: DECIMAL NOT NULL,
-    department_id: INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE employee (
-    id: INT NOT NULL AUTO_INCREMENT = 1000,
-    first_name: VARCHAR(30) NOT NULL,
-    last_name: VARCHAR(30) NOT NULL,
-    role_id: INT NOT NULL,
-    manager_id: INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+   
 );
 
 -- - department
--- databases/seeds.sql
+++ databases/seeds.sql
@@ -5,30 +5,12 @@ INSERT INTO
 VALUES
     ("Accounting"),("Benchtop Assembly & Test"),("Business Intelligence"),("Common Platform"),("Contractor"),("DFM Engineering"),("Encapsulation"),("Emerging Markets"),("Environmental Instruments"),("Laboratory"),("Marketing"),("Research"),("Sales"),("Software"),("Support"),("Telemetry"),("Executive"),("Facilities"),("Failure Analysis"),("Professional Services"),("Information Technology"),("Inventory"),("Kitchen"),("Logistics"),("Machine Shop"),("Operations"),("People Operations"),("Purchasing"),("Repairs");
 
-SELECT * FROM department;
-
 INSERT INTO
     role (title, salary, department_id)
 VALUES
-    ("Business Process Analyst" 70000, 10),("Payroll Specialist", 70000, 10),("Project Manager", 720000, 12),("Continuous Improvement Manager", 80000, 12),("Data Scientist", 90000, 13),("UI/UX Lead", 100000, 13),("Sales Coordinator", 50000, 17),("Director Emerging Markets", 80000, 17),("Director of Instruments", 93000, 18),("Firmware Intern", 60000, 18);
-
-SELECT * FROM role;
+    ("Business Process Analyst", 70000, 1),("Payroll Specialist", 70000, 1),("Project Manager", 720000, 3),("Continuous Improvement Manager", 80000, 3),("Data Scientist", 90000, 4),("UI/UX Lead", 100000, 4),("Sales Coordinator", 50000, 8),("Director Emerging Markets", 80000, 8),("Director of Instruments", 93000, 9),("Firmware Intern", 60000, 9);
 
 INSERT INTO
-    employee (fisrt_name, last_name, role_id, manager_id)
+    employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Jessica", "Day", 100, 1008),("Staci", "Basheer", 101, 1008),("Dillsi", "Flinn", 102, 1008),("Tamsin", "Kinley", 103, 1008),("Jamie", "Jolley", 104, 1008),("Erin", "Ryen", 105, 1008),("Melissa", "McNanny", 106, 1008),("Ping", "Deleon", 107, 1001),("Anthony", "Larsen", 108, 1008),("Ollie", "Sabin", 109, 1008);
-
-SELECT
-    employee.first_name,
-    employee.last_name,
-    role.title,
-    department.name,
-    role.solary,
-    employee.manager_id
-FROM
-    employee
-    LEFT JOIN role ON employee.role_id = role.id
-    LEFT JOIN department ON department.id = role.department_id
-GROUP BY
-    department.id;
+    ("Jessica", "Day", 1, null),("Staci", "Basheer", 2, null),("Dillsi", "Flinn", 3, null),("Tamsin", "Kinley", 4, null),("Jamie", "Jolley", 5, null),("Erin", "Ryen", 6, null),("Melissa", "McNanny", 7, null),("Ping", "Deleon", 8, null),("Anthony", "Larsen", 9, null),("Ollie", "Sabin", 10, null);
